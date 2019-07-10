7c6c43b56e6ef88b5690b529609da1ea0f8e5830
Artem_Dzyubenko <artyom.dz@gmail.com>
2019-07-10 16:31:03 +0600

    Administrator role was added.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,34 +6,3 @@ values ('News number TWO title', TO_DATE('2012-09-17', 'yyyy-mm-dd'), 'News numb
 
 insert into NEWS (NEWS_TITLE, NEWS_CREATE_DATE, NEWS_BRIEF, NEWS_CONTENT, NEWS_USERNAME)
 values ('News number THREE title', TO_DATE('2012-09-17', 'yyyy-mm-dd'), 'News number THREE brief', 'News number THREE content', 'user');
-
------------------------------------------------------
-
-insert into USERS(USERNAME, PASSWORD, ENABLED)
-values('admin','$2a$10$j0brNJfR.ft5dsjSL/42zuKWEkmwGold3RYsUy0lWjzdyR5/yjzlG', 1);
-
-insert into AUTHORITY(USERNAME, AUTHORITY)
-values('admin', 'ROLE_ADMIN');
-
-insert into USERS(USERNAME, PASSWORD, ENABLED)
-values('admin1','e00cf25ad42683b3df678c61f42c6bda', 1);
-
-insert into AUTHORITY(USERNAME, AUTHORITY)
-values('admin1', 'ROLE_ADMIN');
-
-insert into USERS(USERNAME, PASSWORD, ENABLED)
-values('user1','24c9e15e52afc47c225b757e7bee1f9d', 1);
-
-insert into USERS(USERNAME, PASSWORD, ENABLED)
-values('user2','7e58d63b60197ceb55a1c487989a3720', 1);
-
-insert into AUTHORITY(USERNAME, AUTHORITY)
-values('user2', 'ROLE_USER');
-
-insert into USERS(USERNAME, PASSWORD, ENABLED)
-values('user12345','user12345', 1);
-
-insert into AUTHORITY(USERNAME, AUTHORITY)
-values('user12345', 'ROLE_USER');
-
-
